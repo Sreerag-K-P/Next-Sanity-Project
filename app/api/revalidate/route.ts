30bc1362dcd00ab9cb7aa24edc5654ec033b715a
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-20 18:02:23 +0530

    checking webhook

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -6,5 +6,5 @@ export const runtime = "edge";
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get("path") || "/";
   revalidatePath(path);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: true, now: Date.now(), path });
 }
