6892c5ee4f18d6a13addfb2d918c82d7d4fc0369
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-20 21:27:26 +0530

    revalidating

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,14 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-import {} from "sanity";
-
-export const runtime = "edge";
-
 export async function GET(request: NextRequest) {
-  // const path = request.nextUrl.searchParams.get("path") || "";
-  const res = request.json();
-
-  // revalidatePath(path);
-  return NextResponse.json({ revalidated: true, now: Date.now(), res });
+  const path = request.nextUrl.searchParams.get("path") || "/";
+  revalidatePath(path);
+  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
