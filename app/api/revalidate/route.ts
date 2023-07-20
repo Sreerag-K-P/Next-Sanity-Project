79d6a3dc5922832474936cb322c32227bc7c9bc2
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-20 23:38:47 +0530

    /name as path || /

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,7 +3,12 @@ import { revalidatePath } from "next/cache";
 
 export async function GET(req: NextRequest) {
   const body = await req.json();
-  const path = req.nextUrl.searchParams.get(`/${body.name}`) || "/";
-  revalidatePath(path);
+  const data = JSON.parse(body);
+  alert(data);
+  revalidatePath("/");
+  revalidatePath(`//${data.slug}`);
+
+  // const path = req.nextUrl.searchParams.get(`/${body.name}`) || "/";
+  // revalidatePath(path);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
