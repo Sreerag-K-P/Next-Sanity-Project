a19af823adda1eb150d0fa0b05037b3b8511bb84
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-21 00:06:23 +0530

    testing smthing

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 export async function GET(request: NextRequest) {
   const body = await request.text();
 
-  const path = request.headers.get("path") || "/";
+  const path = request.headers.get("path") || "";
   revalidatePath(path);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
