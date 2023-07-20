02fc4e760ead9a69c2442d1b769c9df04c51a17d
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-20 22:30:54 +0530

    name as path

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export async function GET(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get("path") || "/";
+export async function GET(req: NextRequest) {
+  const body = await req.json();
+  const path = req.nextUrl.searchParams.get(body.name) || "";
   revalidatePath(path);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
