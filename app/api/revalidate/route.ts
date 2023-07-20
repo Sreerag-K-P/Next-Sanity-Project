d0b21f5063bc2164ea5f85bfd506b317707d6bbf
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-20 12:24:04 +0530

    checking api/revalidate ...

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
 export async function GET(request: NextRequest) {
-  const tag = request.nextUrl.searchParams.get("tag");
+  const tag = request.nextUrl.searchParams.get("/");
 
   return NextResponse.json(tag);
 }
