a760fa60571a9ac5a45e4fe9b35cdf91332d5fc5
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-20 19:58:22 +0530

    checking api/revalidate

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -7,5 +7,5 @@ export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get("path") || " ";
 
   revalidatePath(path);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: true, now: Date.now(), request });
 }
