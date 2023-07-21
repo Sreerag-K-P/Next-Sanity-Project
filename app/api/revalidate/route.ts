eb89842beb571f736c9da71aeab7b381ea05c512
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-21 19:36:31 +0530

    trying header

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,10 +4,10 @@ import { revalidatePath } from "next/cache";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  const slug = request.body;
-  const path = request.nextUrl.searchParams.get(`/project/${slug}`) || "/";
-  revalidatePath(path);
-  const path2 = request.nextUrl.searchParams.get("join-our-team") || "/";
-  revalidatePath(path2);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  const data = request.body;
+  // const path = request.nextUrl.searchParams.get(`/project/${slug}`) || "/";
+  // revalidatePath(path);
+  // const path2 = request.nextUrl.searchParams.get("join-our-team") || "/";
+  // revalidatePath(path2);
+  return NextResponse.json({ revalidated: true, now: Date.now(), data });
 }
