80b256e2e76b0f54c6a0d1172a1ae3b1c0d57c2a
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-21 14:19:04 +0530

    token used

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,8 +2,9 @@ const config = {
   projectId: "p8vsdvnk",
 
   dataset: "production",
-
+  token: process.env.SANITY_TOKEN,
   apiVersion: "2023-07-07",
+  usCdn: false,
 };
 
 export default config;
