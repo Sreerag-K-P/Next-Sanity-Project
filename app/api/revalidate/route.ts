b3edbc8e081844c8845e176500e94954ac5baf30
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-07-21 19:41:36 +0530

    trying header as request.header

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  const data = request.headers;
+  const data = request.headers.values;
 
   return NextResponse.json({ revalidated: true, now: Date.now(), data });
 }
